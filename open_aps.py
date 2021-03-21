ee0ac5e80af53ad31f8ca16be0f01d24b31c172a
Skinny R&D <jason@skinnyrd.com>
2021-03-21 12:52:35 -0500

    Update open_aps.py

--- open_aps.py
+++ open_aps.py
@@ -2,7 +2,7 @@
 import sqlite3
 import os
 
-file = open('/pineapple/aps.dump','w')
+file = open('/pineapple/Pyneapple_Tools/aps.dump','w')
 
 con = sqlite3.connect('/tmp/recon.db')
 cursor = con.cursor()
@@ -13,4 +13,4 @@ for row in cursor.fetchall():
     file.write(','.join(str(s) for s in row) + '\n')
 file.close()
 
-os.system("""cat aps.dump | cut -c2- | sort | cut -d "," -f 1,2,4 | sort | uniq | grep ",0" | cut -d "," -f 2 > open_aps.txt""")
+os.system("""cat /pineapple/Pyneapple_Tools/aps.dump | cut -c2- | sort | cut -d "," -f 1,2,4 | sort | uniq | grep ",0" | cut -d "," -f 2 > /pineapple/Pyneapple_Tools/open_aps.txt""")
