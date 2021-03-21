1e66aa3d811b56285e0738f24cd394d4cb5c79d0
Skinny R&D <jason@skinnyrd.com>
2021-03-21 12:53:42 -0500

    Update top_ssids.py

--- top_ssids.py
+++ top_ssids.py
@@ -2,7 +2,7 @@
 import sqlite3
 import os
 
-file = open('/pineapple/Activity_Log.dump','w')
+file = open('/pineapple/Pyneapple_Tools/Activity_Log.dump','w')
 
 con = sqlite3.connect('/tmp/log.db')
 cursor = con.cursor()
@@ -11,4 +11,4 @@ for row in cursor.fetchall():
     file.write(','.join(str(s) for s in row) + '\n')
 file.close()
 
-os.system('cat /pineapple/Activity_Log.dump | cut -c3- | sort | cut -d "," -f 1,2| uniq -c | cut -d "," -f 2 | sort | uniq -c | sort -r > top_ssids.txt')
+os.system('cat /pineapple/Pyneapple_Tools/Activity_Log.dump | cut -c3- | sort | cut -d "," -f 1,2| uniq -c | cut -d "," -f 2 | sort | uniq -c | sort -r > /pineapple/Pyneapple_Tools/top_ssids.txt')
